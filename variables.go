af35aa3fdcf69182c362ac9e6fc9d3f27a64c735
Heinrich <smithheinrich254@gmail.com>
2022-07-24 09:17:24 +0300

    variables

--- variables.go
+++ variables.go
@@ -2,8 +2,42 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+)
+
+//at package level we cannot use package colon syntax
+var m int =45
+
+var (
+	actorName string = "Smith Heinrich"
+	companionName string = "Hello"
 )
 
 func main()  {
-	fmt.Println("Varibles")
+	var i int
+	i = 42
+	fmt.Println(i)
+
+	var j int = 43
+	fmt.Println(j)
+
+	k := 44
+	fmt.Println(k)
+	//%v is value while %T is type
+	fmt.Printf("%v,%T",k,k)
+	fmt.Println(m)
+
+	var theHTTP string
+	theHTTP = "facebook.com"
+	fmt.Println(theHTTP)
+	//Type conversion
+
+	var l float32 = 32.5
+	var m int
+	m = int(l)
+	fmt.Printf("%v, %T\n",m,m)
+
+	var o string
+	o = strconv.Itoa(i)
+	fmt.Printf("%v , %T\n",o,o)
 }
